db3a8ce15969ffd317861ccbb2c9bb34ef418f75
Kabasele243 <fkabasele7@gmail.com>
2019-07-18 15:00:37 -0400

    Refactor Navbar Styles and Remove CSS

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-
 import Slider from "rc-slider";
+
 import "rc-slider/assets/index.css";
-import "./NavBar.css";
+import styles from "./styles/NavbarStyles";
 
 class NavBar extends Component {
   constructor(props) {
@@ -25,17 +26,17 @@ class NavBar extends Component {
     this.setState({ open: false });
   }
   render() {
-    const { level, changeLevel, showingAllColors } = this.props;
+    const { level, changeLevel, showingAllColors, classes } = this.props;
     const { format } = this.state;
     return (
-      <header className="Navbar">
-        <div className="logo">
-          <Link to="/#">React Picker</Link>
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
+          <Link to="/">reactcolorpicker</Link>
         </div>
         {showingAllColors && (
-          <div className="slider-container">
+          <div>
             <span>Level: {level}</span>
-            <div className="slider">
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -46,7 +47,7 @@ class NavBar extends Component {
             </div>
           </div>
         )}
-        <div className="select-container">
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
@@ -81,4 +82,4 @@ class NavBar extends Component {
     );
   }
 }
-export default NavBar;
+export default withStyles(styles)(NavBar);
