457c7c80b56927bf32525c3ec5d1f0dc949ebe1a
Kabasele243 <fkabasele7@gmail.com>
2019-07-18 15:14:35 -0400

    refactor NavBar and remove css

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 import styles from "./styles/ColorBoxStyles";
 import { withStyles } from "@material-ui/styles";
 
-import "./ColorBox.css";
-
 class ColorBox extends Component {
   constructor(props) {
     super(props);
